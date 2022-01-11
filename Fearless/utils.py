03abb41340e110e2cc5f0439d4b2badae5fc9ab3
Giovanni Dalmasso <gio.dalmasso@gmail.com>
2022-01-11 15:04:42 +0100

    adding computeAllIntensities.py
    adding file to compute all voxels intensities

--- Fearless/utils.py
+++ Fearless/utils.py
@@ -10,8 +10,10 @@
 # @License: MIT
 # @Copyright: Copyright © 2021 Giovanni Dalmasso
 
+import gc
+import numpy as np
 import os
-from vedo import printc
+from vedo import printc, spher2cart, probePoints
 import shutil
 from sys import exit
 
@@ -41,3 +43,51 @@ def pathExists(path):
             printc("Directory ", path, " Created ", c='green')
         else:
             exit()
+
+
+def voxelIntensity(vol, expo, N, radiusDiscretisation):
+    """Compute voxel intensities."""
+    pos = vol.center()
+    rmax = vol.diagonalSize()/2
+
+    scalars = []
+
+    for th in np.linspace(0, np.pi, N, endpoint=False):
+        for ph in np.linspace(0, 2*np.pi, N, endpoint=False):
+
+            # compute sample points
+            p = spher2cart(rmax, th, ph)
+            samplePointsTmp = []
+            # making discretization more dense away from the center
+            p_tmp = p / (radiusDiscretisation-1)**expo
+            for j in range(radiusDiscretisation):
+                SP = pos + p_tmp * (j**expo)
+                samplePointsTmp.append(SP)
+
+            # compute intensities
+            pb = probePoints(vol, samplePointsTmp)
+
+            del samplePointsTmp
+
+            # making the intensities growing outside the volume according to the gradient
+            scalarsTmp = pb.getPointArray()
+            nonz = np.nonzero(scalarsTmp)[0]
+            if len(nonz) > 2:
+                lastNoZeroId = nonz[-1]  # find the last value != 0
+                secondlastNoZeroId = nonz[-2]
+                # find the last value != 0
+                lastNoZero = scalarsTmp[lastNoZeroId]
+                secondlastNoZero = scalarsTmp[secondlastNoZeroId]
+                dx = lastNoZero - secondlastNoZero
+
+            for i in range(lastNoZeroId+1, len(scalarsTmp)):
+                scalarsTmp[i] = scalarsTmp[i-1] + dx
+            scalars.append(scalarsTmp.tolist())
+
+            del pb, scalarsTmp
+
+    del vol
+    gc.collect()
+
+    # return allIntensitiesMatrix
+    return np.array(scalars).reshape((N * N, radiusDiscretisation))
